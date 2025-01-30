e913b2e65ddd95a9c8a89c0f8de8e55fbed03e82
hpzynha <larissa.nogueira.rocha@gmail.com>
2025-01-30 11:58:18 -0400

    corrigindo a logica

--- src/contactForm.js
+++ src/contactForm.js
@@ -28,45 +28,45 @@ function ContactForm() {
       setStatus(result.message); // exibe a mensagem de sucesso ou erro
     } catch (error) {
       console.log("Erro ao enviar a mensagem", error);
+      setStatus("Erro ao enviar a mensagem");
     }
-    return (
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Nome:</label>
-
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-
-        <div>
-          <label htmlFor="message">Mensagem:</label>
-          <textarea
-            id="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Enviar</button>
-        {status && <div>{status}</div>} //exibe a mensagem de status
-      </form>
-    );
   };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Nome:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="message">Mensagem:</label>
+        <textarea
+          id="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit">Enviar</button>
+      {status && <div>{status}</div>} {/* exibe a mensagem de status */}
+    </form>
+  );
 }
 
 export default ContactForm;
